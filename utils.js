47f6ac5b35af8888c6d9f90a11fd5a2e004cfd21
jai <jai@navanatech.in>
2025-06-28 10:17:40 +0530

    chore: removed unnecessary files

--- utils.js
+++
@@ -1,8 +0,0 @@
-export function parseCommand(text) {
-  if (!text.toLowerCase().startsWith('bb')) return null;
-  const parts = text.trim().split(/\s+/);
-  return {
-    cmd: parts[1]?.toLowerCase(),
-    args: parts.slice(2)
-  };
-}
